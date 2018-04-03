ce918797fb56c3e31c8e7f6307c30d5257cb1530
WataruOhnishi <WataruOhnishi@users.noreply.github.com>
2018-04-03 22:32:22 +0900

    initial update
    4th order

---
+++ P4th/bench_4th.m
@@ -0,0 +1,45 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 4th order benchmark problem:
+% ------------------------
+% Descr.:   Benchmark problem for two MP systems and one NMP system 
+% System:   4th order model of nano stage 1 
+% Author:   Wataru Ohnishi, Koseki lab, the University of Tokyo, 2018
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear; clc;
+close all;
+
+s = tf('s');
+%% plant definition
+Pmp1 = func_plant_stage1(0); % minimum phase system (collocated)
+Pmp2 = func_plant_stage1(0.085); % minimum phase system (non-collocated) 
+Pnmp = func_plant_stage1(0.30); % non-minimum phases system (non-collocated)
+
+% 2nd order approximation
+[z_Pmp1, p_Pmp1, k_Pmp1] = zpkdata(Pmp1,'v');
+P2 = zpk([],[p_Pmp1(1),p_Pmp1(4)],k_Pmp1)/dcgain(zpk(p_Pmp1(2:3),z_Pmp1,1));
+figure;  bode(P2,Pmp1,Pmp2,Pnmp);
+
+%% Pmp1
+P = Pmp1;
+C = 1; % please design controller
+
+% verify
+% reference response
+Gyr = feedback(P*C,1);
+figure; step(Gyr);
+
+% disturbance response
+Gyd = feedback(P,C);
+figure; step(Gyd);
+
+% sensitivity function
+S = feedback(1,P*C);
+figure; bode(S);
+
+% margin
+M = allmargin(P*C)
+
+% % nyquist diagram
+% figure; nyquist(P*C);
+
---
+++ P4th/func_plant_stage1.m
@@ -0,0 +1,27 @@
+﻿function sys_c = func_plant_stage1(l) % l: height of the measurement point
+
+% Fujimoto, H., Sakata, K., & Saiki, K. (2010). Application of Perfect Tracking Control to Large-Scale High-Precision Stage. In 5th IFAC Symposium on Mechatronic Systems (pp. 188–193).
+% Hara, A., Saiki, K., Sakata, K., & Fujimoto, H. (2008). Basic examination on simultaneous optimization of mechanism and control for high precision single axis stage and experimental verification. In 34th Annual Conference of IEEE Industrial Electronics (pp. 2509–2514).
+
+syms s_syms
+syms M m J mu_theta C k_theta 
+syms L g l_sym
+
+a4 = M*m*L^2 + M*J + m*J;
+a3 = M*mu_theta + m*mu_theta + (m*L^2 + J)*C;
+a2 = M*k_theta + m*k_theta - M*m*g*L - m^2*g*L + mu_theta*C;
+a1 = (k_theta - m*g*L)*C;
+b2 = m*L^2 + J - m*L*l_sym;
+b1 = mu_theta;
+b0 = k_theta - m*g*L;
+
+Pn = (b2*s_syms^2 + b1*s_syms + b0)/(a4*s_syms^4 + a3*s_syms^3 + a2*s_syms^2 + a1*s_syms);
+Pn_subs = subs(Pn,{M,m,J,C,k_theta,mu_theta,L,g},...
+    {7.7,5.3,0.015,24,1700,0.20,0.092,9.8});
+% Dr. Sakata thesis p.107
+Pn_subs = subs(Pn_subs,l_sym,l); 
+
+Pn_c = sym2tf(Pn_subs);
+
+sys_c = Pn_c;
+
